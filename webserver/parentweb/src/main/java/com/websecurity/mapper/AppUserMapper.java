3428233ebaf0bfa59088e0292a4b75d7bce45bbe
himanshu <qwert12345>
2018-03-18 17:47:30 +0530

    some additions - spring security

---
+++ webserver/parentweb/src/main/java/com/websecurity/mapper/AppUserMapper.java
@@ -0,0 +1,24 @@
+package com.websecurity.mapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import com.websecurity.models.AppUser;
+import org.springframework.jdbc.core.RowMapper;
+
+public class AppUserMapper implements RowMapper<AppUser> {
+
+    public static final String BASE_SQL //
+            = "select u.user_id, u.user_name, u.encrypted_password From app_user u ";
+
+    @Override
+    public AppUser mapRow(ResultSet rs, int rowNum) throws SQLException {
+
+        Long userId = rs.getLong("user_id");
+        String userName = rs.getString("user_name");
+        String encrytedPassword = rs.getString("encrypted_password");
+
+        return new AppUser(userId, userName, encrytedPassword);
+    }
+
+}
